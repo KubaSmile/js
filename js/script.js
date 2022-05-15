b9a727ce1b5865cf9aaf0d45de9ea0d4ddce1479
ivan <Freekubasmile@gmail.com>
2022-05-15 20:06:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log(typeof(answer));
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('сколько вам лет?', '');
+// answers[2] = prompt('вы чайник?', '');
+
+console.log(typeof(answers));
